f90a00bf61ce78c844663ddb3cd8a7f46ce32ec3
Ajmal2025 <139298518+Ajmal2025@users.noreply.github.com>
2023-10-25 21:47:50 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+"use strict";
+//8.	You should create four lines that look like this:
+//________________________________________
+//console.log(5 + 3)
+//________________________________________
+//Your output should simply be four lines with the number 8 appearing once on each line.
+console.log(5 + 3);
+console.log(16 / 2);
+console.log(16 - 8);
+console.log(2 * 4);
